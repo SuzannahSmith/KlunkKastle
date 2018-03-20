b34f4e1848c58fafcf5d35ff333b19e16bdb87ef
Suzannah Smith <suzannah.m.smith@gmail.com>
2018-03-20 18:29:05 -0300

    Worked on level 2 animations and obstacles

---
+++ Assets/Scripts/AnimationOffset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOffset : MonoBehaviour {
+
+	void Start () {
+		Animator anim = GetComponent<Animator>();
+		anim.Play("hammer", -1, 0.5f);
+	}
+
+}
