815601afd633da81d811eaf8469e3fef7f0aa00c
Suzannah Smith <suzannah.m.smith@gmail.com>
2018-03-31 21:03:56 -0300

    Added some menu controller stuff

---
+++ Assets/MenuControllerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void NewGame() {
+		Debug.Log("quiting game");
+	}
+
+	public void QuitGame() {
+
+	}
+
+	public void SelectLevel() {
+
+	}
+
+	public void StartTutorial() {
+
+	}
+}
