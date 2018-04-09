0c3097e3a9f5c3e0679cf59dcf7357f052059f0b
Suzannah Smith <suzannah.m.smith@gmail.com>
2018-04-09 10:16:53 -0300

    Fixed some minor details

--- Assets/Scripts/StoryTextController.cs
+++ Assets/Scripts/StoryTextController.cs
@@ -18,7 +18,7 @@ public class StoryTextController : MonoBehaviour {
 		storyTextList = new string[] {
 			"Glorbie, a little Glorb child, lives a happy life in Glorbland. He is the heir to the Glorb throne, and his parents King Glorbster and Queen Glorbella love him very much. Everything is perfect….",
 			 "Until the day that the Klunks attack.",
-			 "The Klunks are jealous of the riches of that the Glorbs have gathered by making gum balls. The Klunks can only make black licorice, which nobody likes. So, they came up with a plan to steal the Glorbs' gum balls: by kidnapping innocent little Glorbie and holding him for ransom!",
+			 "The Klunks are jealous of the riches that the Glorbs have gathered by making gum balls. The Klunks can only make black licorice, which nobody likes. So, they came up with a plan to steal the Glorbs' gum balls: by kidnapping innocent little Glorbie and holding him for ransom!",
 			 "In the dead of the night, a group of Klunks snuck into Glorbland and snatched Glorbie out of his bed. They took him back to Klunk Kastle, and hid him away on the highest floor of the Kastle. There he will stay, until the Glorbs pay a ransom for him, of one billion gum balls.",
 			 "But still, Glorbie has no intention of waiting for his parents to rescue him. He isn't going to let anyone take his precious gum balls away! He sets out to escape the Kastle himself… Despite the danger.",
 			 "Klunk Kastle is a very scary place. Almost everything in it is designed to kill any Glorbs who dare enter it! He will need a lot of help to make it out alive.",
